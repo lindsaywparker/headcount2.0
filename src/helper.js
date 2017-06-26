bcd2ecafcb600b3829fdbb625b490476ad18dc44
Lindsay Parker <lindsay.w.parker@gmail.com>
2017-06-26 17:10:06 -0600

    Pass Iteration 0 tests, reformat incoming data

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,19 @@
 export default class DistrictRepository {
+  constructor(dataset) {
+    // this.camelCaseData = dataset.map( elem => {
+    //   let keys = Object.keys(elem).map( key => {
+    //     return key;
+    //   });
+    //   
+    //   // console.log(keys);
+    //   return {};
+    // });
+    
+    this.data = dataset.reduce( (acc, elem) => {
+        acc[elem.Location] = Object.assign({}, acc[elem.Location], { [elem.TimeFrame]: elem.Data });
+      return acc;
+    }, {});
+    
+  }
 
 }
