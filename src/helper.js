eaeb881980ad8644d4e097367ec2f671b89adbf9
Joe Heitkamp <noetic97@gmail.com>
2017-06-27 14:07:19 -0600

    WIP - itereration 1 tests

--- src/helper.js
+++ src/helper.js
@@ -1,29 +1,33 @@
 export default class DistrictRepository {
   constructor(dataset) {
-    // this.camelCaseData = dataset.map( elem => {
-    //   let keys = Object.keys(elem).map( key => {
-    //     return key;
-    //   });
-    //   
-    //   // console.log(keys);
-    //   return {};
-    // });
-    
-    this.data = dataset.reduce( (acc, elem) => {
-        acc[elem.Location] = Object.assign({}, acc[elem.Location], { [elem.TimeFrame]: elem.Data });
-      return acc;
-    }, {});
-    
+    this.data = this.scrubData(dataset)
   }
-  
+
   findAllMatching() {
     const allResults = [];
     return allResults;
   }
-  
-  findByName() {
-    const byNameResults = 'hi';
+
+  findByName(input) {
+    if(!input) {
+      return undefined;
+    }
+
+    input = input.toUpperCase();
+
+    if(!this.data[input]) {
+      return undefined;
+    }
+
+    const byNameResults = Object.assign({}, {location: input.toUpperCase()});
     return byNameResults;
   }
 
+  scrubData(data) {
+    return data.reduce( (acc, elem) => {
+      elem.Location = elem.Location.toUpperCase();
+        acc[elem.Location] = Object.assign({}, acc[elem.Location], { [elem.TimeFrame]: elem.Data });
+      return acc;
+    }, {});
+  }
 }
