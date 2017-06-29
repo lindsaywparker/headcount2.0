d9118e463ebe4e7d3ae9a15703900c7b667c7649
Lindsay Parker <lindsay.w.parker@gmail.com>
2017-06-29 17:46:08 -0600

    Add comparison results, closes #19

--- src/Comparison.js
+++ src/Comparison.js
@@ -3,7 +3,7 @@ import Card from './Card';
 import './Comparison.css';
 
 
-const Comparison = ({ dataArray, toggleSelected }) => {
+const Comparison = ({ dataArray, toggleSelected, findAverage, compareDistrictAverages }) => {
   const selectedDistricts = dataArray.filter((district) => {
     return district.selected;
   })
@@ -11,15 +11,18 @@ const Comparison = ({ dataArray, toggleSelected }) => {
   let showOne = false;
   let showComp = false;
   let showTwo = false;
+  let comparisonResults;
 
   if(selectedDistricts.length >= 1) {
     showOne = true;
   }
+  
   if(selectedDistricts.length >= 2) {
     showComp = true;
     showTwo = true;
+    comparisonResults = compareDistrictAverages(selectedDistricts[0].location, selectedDistricts[1].location)
   }
-
+  
   return (
     <div className='comparison'>
       {showOne && <Card key={0}
@@ -28,7 +31,11 @@ const Comparison = ({ dataArray, toggleSelected }) => {
         selected={selectedDistricts[0].selected}
         toggleSelected={toggleSelected}
       />}
-      {showComp && <p></p>}
+      {showComp && <div>
+        <p>{selectedDistricts[0].location} AVERAGE: {(comparisonResults[selectedDistricts[0].location] * 100).toFixed(1)}%</p>
+        <p>Ratio (Location 1 / Location 2): {(comparisonResults.compared * 100).toFixed(1)}%</p>
+        <p>{selectedDistricts[1].location} AVERAGE: {(comparisonResults[selectedDistricts[1].location] * 100).toFixed(1)}%</p>
+      </div>}
       {showTwo && <Card key={1}
         location={selectedDistricts[1].location}
         data={selectedDistricts[1].data}
